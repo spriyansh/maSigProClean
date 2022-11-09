6b0c1a30de6496e67e5f40255e73dccf6ace6e4d
Priyansh Srivastava <spriyansh29@gmail.com>
2022-11-09 18:06:10 +0100

    Added comments for p.vector()

--- R/p.vector.R
+++ R/p.vector.R
@@ -1,48 +1,47 @@
-p.vector <- function(
-        data, # Expected Single Cell Count Table
-        design, # Internal Design File made previously # Accepts list,
-        # data.frame, and matrix #--Fix required: Restrict input to maSigPro internal design file
-        Q = 0.05, # significance level
-        MT.adjust = "BH", # Depends on library(stats) p.adjust
-        min.obs = 6,
-        counts = FALSE, # Logical to indicate if the data are counts
-        family = NULL,
-        theta = 10,
-        epsilon = 1e-05,
-        item = "gene") {
-    
-    # Condition-1: Check the datatype for the design file
-    # If the supplied file is a dataframe or a matrix, 'groups.vector' and 'edesign'
-    # will have null invoke
+p.vector <- function(data, # Expected Single Cell Count Table
+                     design, # Internal Design File made previously # Accepts list,
+                     # data.frame, and matrix #--Fix required: Restrict input to maSigPro internal design file
+                     Q = 0.05, # significance level
+                     MT.adjust = "BH", # Depends on library(stats) p.adjust
+                     min.obs = 6,
+                     counts = FALSE, # Logical to indicate if the data are counts
+                     family = NULL,
+                     theta = 10,
+                     epsilon = 1e-05,
+                     item = "gene") {
+
+  # Condition-1: Check the datatype for the design file
+  # If the supplied file is a dataframe or a matrix, 'groups.vector' and 'edesign'
+  # will have null invoke
   if (is.data.frame(design) || # if it is a dataframe
-      is.matrix(design)) {# if it is a matrix
-      
-      # Internal Design file == User supplied Matrix or Data.frame
+    is.matrix(design)) { # if it is a matrix
+
+    # Internal Design file == User supplied Matrix or Data.frame
     dis <- design
-    
+
     # Invoke NULL
     groups.vector <- NULL
-    
+
     # Invoke NULL
     edesign <- NULL
-    
+
     # Else if a list from make.matrix.design()
     # Check list structure #---Fix: Better check has to be performed
   } else if (is.list(design)) {
-      
-      # Assign internal design file
+
+    # Assign internal design file
     dis <- as.data.frame(design$dis)
-    
+
     # Assign group vectors
     groups.vector <- design$groups.vector
-    
+
     # Add original experimental.design file
     edesign <- design$edesign
   }
 
-    # Conditio-2; Check for Residual Family
-    # For single cell we will need negative binomial
-    # Selects negative binomial if counts == T
+  # Conditio-2; Check for Residual Family
+  # For single cell we will need negative binomial
+  # Selects negative binomial if counts == T
   if (is.null(family)) { # Default gaussian
     if (!counts) { # if not equal to null
       family <- gaussian() # Gaussian family
@@ -52,24 +51,24 @@ p.vector <- function(
     }
   }
 
-    # Data to matrix conversion
-    #-Fix: Check it before hand
+  # Data to matrix conversion
+  #-Fix: Check it before hand
   dat <- as.matrix(data)
-  
+
   # Re-ordering via rownames
   # This will also drop the cell/ samples which donot exist in metadata
   #---Bug--: Will not work if the internal design file will have extra cells
   dat <- dat[, as.character(rownames(dis))]
-  
+
   # Number of genes in the count matrix
   G <- nrow(dat)
 
   # Removing rows with many missings:
-  # This will count the number of missing values 
+  # This will count the number of missing values
   #---Warning: Bux fix required for some cases: Only NA is counted
   # ScRNA-Seq has many 0s, which is not counted here
   count.na <- function(x) (length(x) - length(x[is.na(x)]))
-  
+
   # Subset the counts, aginst the min.obs, default == 6
   dat <- dat[apply(dat, 1, count.na) >= min.obs, ]
 
@@ -81,12 +80,12 @@ p.vector <- function(
   if (length(counts0) > 0) {
     dat <- dat[-counts0, ]
   }
-  
+
   # Save attributes of the final dataframe/ counts
   g <- dim(dat)[1] # Number of Genes/ Rows #--- better syntax:  g <- nrow(dat)
   n <- dim(dat)[2] # Number of cells/ Columns  #--- better syntax:  n <- ncol(dat)
   p <- dim(dis)[2] # Number of terms of the model or group vectors
-  
+
   # Invoke vector equivalent to length of genes
   # Not necessary to invoke a defined length
   p.vector <- vector(mode = "numeric", length = g)
@@ -96,9 +95,9 @@ p.vector <- function(
   # Run for loop on each gene one by one
   # g is the total number of genes
   for (i in 1:g) {
-      
+
     # Subset one gene at a time
-      # Store in Variable y for modelling
+    # Store in Variable y for modelling
     y <- as.numeric(dat[i, ])
 
     #---Bad syntax to print progress**************************************
@@ -111,26 +110,29 @@ p.vector <- function(
 
     # Actual model fitting
     model.glm <- glm(y ~ ., # Formula all the coviarates
-                     data = dis,
-                     family = family, 
-                     epsilon = epsilon)
-    
-    # print(model.glm)
-    # print(model.glm$null.deviance)
-    
-    # Condition: 
+      data = dis, # Only fit on time values
+      family = family, # Negative binomial family
+      epsilon = epsilon # Intercept term
+    )
+
+    # Condition: check if null deviance ==0
     if (model.glm$null.deviance == 0) {
+      # If null deviance === 0 the p value vector will be 1
+      # this will later discard the model from the analysis
+      print(model.glm)
+      stop()
       p.vector[i] <- 1
     } else {
-      model.glm.0 <- glm(y ~ 1,# Formula with just intercept
-                         family = family,
-                         epsilon = epsilon
-                         )
-      
-      print(model.glm.0)
-      print(model.glm.0$null.deviance)
-      stop()
+      model.glm.0 <- glm(y ~ 1, # Formula with just intercept
+        family = family, # Negative binomial family
+        epsilon = epsilon # Intercept term
+      )
 
+
+      # Condition for Gaussian distribution
+      # Unused for single cell data
+      # Remove from the analysis
+      #---------------------------------------------------------
       if (family$family == "gaussian") {
         test <- anova(model.glm.0, model.glm, test = "F")
         if (is.na(test[6][2, 1])) {
@@ -138,33 +140,68 @@ p.vector <- function(
         } else {
           p.vector[i] <- test[6][2, 1]
         }
+        #-----------------------------------------------------------
+
+
+        # Condition for negative binomial
+        # Convert to GAM
+        #-------------------------
       } else {
+
+        # Model adquaecy tetsing with ANoVA
+        # Analysis of deviance table
         test <- anova(model.glm.0, model.glm, test = "Chisq")
+
+        # Check for significance level
+        # Improve synatx for this section
+        #------------------------------------------
         if (is.na(test[5][2, 1])) {
+          # If significance is NA then the p value vector will be 1
+          # this will later discard the model from the analysis
           p.vector[i] <- 1
+          stop()
         } else {
+          # Saving significance if not null
           p.vector[i] <- test[5][2, 1]
-        }
-      }
-    }
-    
+        } #-------------------------------------------
+      } # Close Condition for negative binomial
+    } # Close condition for null deviance ==0
+
     # Model fitting outer loop close
   }
 
   #----------------------------------------------------------------------
   # P-value adjustment with stats package
-  p.adjusted <- p.adjust(p.vector, method = MT.adjust, n = length(p.vector))
+  p.adjusted <- p.adjust(p.vector, # Supply p value vector after for all the models
+    method = MT.adjust, # Method for p-va;ue correction
+    n = length(p.vector)
+  ) # Donot change this # stats package
+
+  # Since the p.vector is ordered according to the dataframe
+  # each p value will map to a gene
+  # selecting significant genes (0.05) will enable mapped selection
+  # of the gene from the counts
+  #---Improve synatx needed here
   genes.selected <- rownames(dat)[which(p.adjusted <= Q)]
-
   FDR <- sort(p.vector)[length(genes.selected)]
-
   SELEC <- as.matrix(as.data.frame(dat)[genes.selected, ])
+
+  # If none of the gene is selected
+  # Program still continues here by throwing an error
+  #---Improved syntax needed here
   if (nrow(SELEC) == 0) {
     print("no significant genes")
   }
 
+  #---Better syntax needed
+  # p.vector should be a named list not a matrix
   p.vector <- as.matrix(p.vector)
+
+  # Rownames will correspond to genes
   rownames(p.vector) <- rownames(dat)
+
+  # Custom column name
+  # Improve notation of column by "p_value"
   colnames(p.vector) <- c("p.value")
 
   #-------------------------------------------------------------------------
@@ -172,13 +209,13 @@ p.vector <- function(
   # return named list of objects
   # Make list of object made
   output <- list(SELEC, p.vector, p.adjusted, G, g, FDR, nrow(SELEC), dis, dat, min.obs, Q, groups.vector, edesign, family)
-  
+
   # Assign name
   names(output) <- c(
     "SELEC", "p.vector", "p.adjusted", "G", "g", "FDR", "i", "dis", "dat", "min.obs", "Q", "groups.vector", "edesign",
     "family"
   )
-  
+
   #---Syntax-fix needed: Use proper retun statement, return(output)
   output
 }
